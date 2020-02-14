446d59ebe4ee2bac252171ed7c0d9e2e6615ec41
LegPains <55473915+LegPains@users.noreply.github.com>
2020-02-14 20:30:00 +0800

    Add files via upload
    Writes to CSV (first->article, second->all comments)
    Scrapes data while crawling to reduce document.get frequency (slowest part of the code)
    increased blacklisted terms

--- Web Crawler/src/webcrawler/Soup.java
+++ Web Crawler/src/webcrawler/Soup.java
@@ -1,9 +1,11 @@
 package webcrawler;
 
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,13 +14,18 @@ import org.jsoup.select.Elements;
 
 public class Soup extends WebCrawler{
 	private ArrayList<String> blacklist;
+	private String keyword;
 	
-	//Constructor - place base URL and depth to start
+	//Constructor - place base URL and depth
 	public Soup(String base, int depth) {
 		super(base, depth);
 		this.blacklist = new ArrayList<String>();
+		this.addBlacklist("#", "cars", "civis", "store", "gaming", "video", "staff", 
+				"technopaedia", "author", "start=", "uploads", "tag", "mail", "video", 
+				"apple", "advert", "reprints", "newsletters", "about-us", "rss", "conde", "reviews",
+				"?view", "staff-directory", "?theme");
+		this.keyword = new String();
 	}
-	
 	
 	//get list of URL that do not include the tag
 	public ArrayList<String> getBlacklist() {
@@ -31,6 +38,14 @@ public class Soup extends WebCrawler{
 		this.blacklist.add(key);
 	}
 	
+	public String getKeyword() {
+		return this.keyword;
+	}
+	
+	public void setKeyword(String s) {
+		this.keyword = s;
+	}
+	
 	//crawl base URL provided
 	public void crawl(String url, int lowerDepth) {
 		//checks list if already crawled, lesser than MAX_DEPTH, and if base link is present
@@ -38,15 +53,16 @@ public class Soup extends WebCrawler{
 			//Grabs links from sites recursively
 			try {
 				for(String word:this.getBlacklist()) {
-				if(url.contains(word)) {
-					return;
+					if(url.contains(word)) {
+						return;
 					}
 				}
 				//add current URL to ArrayList - prevent infinite loop
 				this.addURLs(url);
 				Document site = Jsoup.connect(url).get();
-				System.out.printf("Depth %d: Crawling %s\n", lowerDepth, site.title());
-				System.out.printf("%s\n", url);
+				System.out.printf("Crawling Depth %d: %s\nURL: %s\n", lowerDepth, site.title(), url);
+				//grab Data here if site has keyword to avoid going through the whole list a second time
+				getData(keyword, site);
 				//select all <a> tags
 				Elements linksOnPage = site.select("a[href]");
 				lowerDepth++;
@@ -57,57 +73,64 @@ public class Soup extends WebCrawler{
 			}
 			catch(IOException e) {
 				System.err.println("Exception while crawling: "+ e.getMessage() + " - " + url);
-				}
+			}
 		}
 	}
 	
 	//grab data from current URL site with reference to keyword and the tags
-	//FILTER ?COMMENTS=1
-	public void getData(String keyword, String tag) {
-		ArrayList<String> parsedList = this.getURLs();
-		//loop through parsedList to scrape data
-		for(String url : parsedList)
-			try {
-				Document site = Jsoup.connect(url).get();
-				Elements data = site.select(tag);
-				//if tag does not exist, blacklist the URL and move to next
-				if(data == null) {
-					this.addBlacklist(url);
-					throw new IOException("Tag not found - Adding to blacklist");
-				}
-				else if(!blacklist.contains(url)) {
-					//if data does not contain keyword, throw exception
-					if(data.text().contains(keyword)) {
-						writeToFile(url);
-						writeToFile(site.title());
-						writeToFile(data.text());
-					}
-					else {
-						throw new IOException("Keyword not found within comments");
-					}
-				}
-				else {
-					throw new IOException("Skipping blacklisted site: " + url);
-				}
+	public void getData(String keyword, Document site) {
+	//loop through past URLs to scrape data
+		try {
+			Elements article = site.select("div.article-content");
+			//Elements name = site.select("li.comment > header");
+			Elements data = site.select("li.comment");
+			//if data does not contain keyword, throw exception
+			if(article.text().contains(keyword)) {
+				System.out.println("Parsing " + site.title());
+				ArrayList<String[]> CSVbuffer = new ArrayList<String[]>();
+				CSVbuffer.add(new String[] {article.text(), data.text()});
+				writeToCSV(CSVbuffer);
 			}
-			catch(IOException e) {
-				System.err.println("Exception while parsing: " + e.getMessage());
+			else {
+				throw new IOException("Keyword or tag not found, skipping site");
 			}
+		}
+		catch(IOException e) {
+			System.err.println("Exception while parsing: " + e.getMessage());
+		}
 	}
 	
 	//creates an output file and writes to it
-	//ARRANGE IT - bufferedreader
-	public void writeToFile(String s) {
-		PrintWriter text = null;
+	public void writeToCSV(ArrayList<String[]> s) throws IOException {
+		FileWriter output = new FileWriter("./output.csv", true);
 		try {
-			text = new PrintWriter("output.txt");
-			text.println(s);
-		}
-		catch(FileNotFoundException e) {
-			System.err.println("Exception while writing to file: " + e.getMessage());
+			PrintWriter pw = new PrintWriter(output);
+			s.stream()
+				.map(this::convertToCSV)
+				.forEach(pw::println);
+			pw.close();
 		}
 		finally {
-			text.close();
+			
 		}
+		
 	}
+	
+	//helper function to write to CSV
+	public String convertToCSV(String[] data) {
+	    return Stream.of(data)
+	      .map(this::escapeSpecialCharacters)
+	      .collect(Collectors.joining(","));
+	}
+	
+	//used to escape special characters when writing to csv
+	public String escapeSpecialCharacters(String data) {
+	    String escapedData = data.replaceAll("\\R", " ");
+	    if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+	        data = data.replace("\"", "\"\"");
+	        escapedData = "\"" + data + "\"";
+	    }
+	    return escapedData;
+	}
+	
 }
